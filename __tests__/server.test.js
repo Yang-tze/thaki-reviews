9049f5efddedf0a7faa3b355b9491243a5cada3e
Cheyenne Noel <cheyennenoel@gmail.com>
2018-07-20 12:16:22 -0700

    Set up jest, babel

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,5 +1,24 @@
-describe('Addition', () => {
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// import React from 'react';
+// import sinon from 'sinon';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Client', () => {
   it('knows that 2 and 2 make 4', () => {
     expect(2 + 2).toBe(4);
   });
+  // it('knows that 2 and 2 make 4', () => {
+  //   expect(2 + 2).toBe(4);
+  // });
+  // it('knows that 2 and 2 make 4', () => {
+  //   expect(2 + 2).toBe(4);
+  // });
+  // it('knows that 2 and 2 make 4', () => {
+  //   expect(2 + 2).toBe(4);
+  // });
+  // it('knows that 2 and 2 make 4', () => {
+  //   expect(2 + 2).toBe(4);
+  // });
 });
