a2605cc378567ac2aba76f937e1090164c92f623
Cheyenne Noel <cheyennenoel@gmail.com>
2018-07-22 10:16:45 -0700

    Fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    path: path.resolve(__dirname, 'src'),
-    filename: 'index.js'
-  },
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
+            presets: ['react','env'],
+          },
+        },
+      },
+    ],
+  },
 };
