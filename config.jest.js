a67a5ba814ec13005c7c32fc76096608be89e918
Cheyenne Noel <cheyennenoel@gmail.com>
2018-07-22 10:38:24 -0700

    Split component declaration from component render files

--- config.jest.js
+++ config.jest.js
@@ -5,10 +5,10 @@ module.exports = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'js', 'jsx'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/spec/__mocks__/fileMock.js',
-    '\\.css$': 'identity-obj-proxy'
+    '\\.css$': 'identity-obj-proxy',
   },
   testPathIgnorePatterns: [
     '<rootDir>/public/bundle.js',
-    '<rootDir>/node_modules'
+    '<rootDir>/node_modules',
   ]
 };
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Service from './service.jsx';
 
-const Service = () => (
-  <div>
-    Service Component
-  </div>
-);
-
 ReactDOM.render(<Service />, document.getElementById('service'));
