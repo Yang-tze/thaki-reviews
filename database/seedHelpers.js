ca7dd8a1a22e884f5e6b7152d5608e9667edb8a2
Cheyenne Noel <cheyennenoel@gmail.com>
2018-07-22 14:22:37 -0700

    Implement generateReview

--- database/seedHelpers.js
+++ database/seedHelpers.js
@@ -6,21 +6,28 @@ import {
 } from './loadAssets.js';
 
 const inclusiveRandom = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const minR = Math.ceil(min);
+  const maxR = Math.floor(max);
+  return Math.floor(Math.random() * (maxR - minR + 1)) + minR;
+};
+
+const insertUser = (user) => {
+  db.query(`INSERT INTO users (username, img) VALUES("${user.username}", "${user.img}"); SELECT id FROM USERS WHERE username="${user.username}";`, (err, data) => {
+    if (err) throw err;
+    console.log('The user is: ', data);
+    return data;
+  });
 };
 
 const createUser = () => {
   const user = {};
   const startIndex = inclusiveRandom(0, 100);
-  const endIndex = inclusiveRandom(startIndex + 5, startIndex + 100);
-  user.username = hipsum[inclusiveRandom(0, 14)].slice(startIndex, endIndex);
+  user.username = hipsum[inclusiveRandom(0, 14)].slice(startIndex).split(' ')[0];
   if (inclusiveRandom(1, 3) === 3) {
     user.img = profilePics[inclusiveRandom(0,25)];
   }
-  db.query('INSERT INTO users (username, img) VALUES("' + user.username + '", "' + user.img + '"); SELECT id FROM USERS WHERE username="' + user.username + '";', (err, data) => {
-    if (err) return createUser();
+  db.query(`SELECT id FROM users WHERE username="${user.username}";`, (err, data) => {
+    if (err) insertUser(user);
     console.log('The user is: ', data);
     return data;
   });
@@ -41,12 +48,25 @@ const assignUser = () => {
   return createUser();
 };
 
-const generateTitle = () {
-
+const generateTitle = () => {
+  const startIndex = inclusiveRandom(0, 100);
+  const endIndex = inclusiveRandom(startIndex + 5, startIndex + 100);
+  return hipsum[inclusiveRandom(0, 14)].slice(startIndex, endIndex);
 };
 
-const generateReview = () {
-
+const generateReview = () => {
+  let review = '';
+  // const length = inclusiveRandom(1, 5);
+  hipsum.forEach((paragraph) => {
+    if (inclusiveRandom(1, 7) === 3) {
+      review = `${review} ${paragraph} \n`;
+      // const sentences = paragraph.split('.');
+      // sentences.forEach((sentence) => {
+      //   inclusiveRandom(1, 5) === 3 ? review.push(sentence);
+      // })
+    }
+  });
+  return review.slice(0, review.length - 3);
 };
 
 const addImages = (review) => {
