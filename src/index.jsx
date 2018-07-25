ab04749669164a0a339f8b5e67b48d6b5d3a3ef6
Cheyenne Noel <cheyennenoel@gmail.com>
2018-07-25 15:39:13 -0700

    Update config.yml

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import Service from './service.jsx';
+import reviews from './reducers'
 
-ReactDOM.render(<Service />, document.getElementById('service'));
+const store = createStore(reviews);
+
+ReactDOM.render(<Service store={store}/>, document.getElementById('service'));
--- src/service.jsx
+++ src/service.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { METHODS } from 'http';
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import App from './App'
+​
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <Route path="/:filter?" component={App} />
+    </Router>
+  </Provider>
+)
+​
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+}
+
 
 class Service extends React.Component {
   constructor(props) {
