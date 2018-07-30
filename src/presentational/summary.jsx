ab158c24a22816d8d149488f3df922347ca912ef
Cheyenne Noel <cheyennenoel@gmail.com>
2018-07-30 09:00:42 -0700

    Implement histogram spacing

--- src/presentational/summary.jsx
+++ src/presentational/summary.jsx
@@ -14,37 +14,38 @@ const Summary = ({ aggregates }) => {
   const one = Math.round(Number(aggregates.one) / Number(aggregates.qty) * 100) || 0;
   const stringPercent = (percent) => {
     const string = percent.toString();
-    if (string.length === 3) return string;
-    if  (string.length === 2) return ` ${string}`;
-    return `  ${string}`;
-  }
+    if (string.length === 3) return `${string}%`;
+    if (string.length === 2) return `${string}% `;
+    return `${string}%  `;
+  };
   return (
     <div className={styles.link && styles.summary}>
-      {aggregates.score} out of 5 stars{/*Hover modal*/} &#129170;<br></br>
+      {aggregates.score} out of 5 stars{/*Hover modal*/} &#129170;
+      <br></br>
       <div className={styles.progressbar}>
         <div className={styles.rating}>5 star </div>
         <ProgressBar percentage={five} rating={5} />
-        <div className={styles.percent}> {stringPercent(five)}% </div>
+        <div className={styles.percent}> {stringPercent(five)}</div>
       </div>
       <div className={styles.progressbar}>
         <div className={styles.rating}>4 star </div>
         <ProgressBar percentage={four} rating={4} />
-        <div className={styles.percent}> {stringPercent(four)}% </div>
+        <div className={styles.percent}> {stringPercent(four)}</div>
       </div>
       <div className={styles.progressbar}>
         <div className={styles.rating}>3 star </div>
         <ProgressBar percentage={three} rating={3} />
-        <div className={styles.percent}> {stringPercent(three)}% </div>
+        <div className={styles.percent}> {stringPercent(three)}</div>
       </div>
       <div className={styles.progressbar}>
         <div className={styles.rating}>2 star </div>
         <ProgressBar percentage={two} rating={2} />
-        <div className={styles.percent}> {stringPercent(two)}% </div>
+        <div className={styles.percent}> {stringPercent(two)}</div>
       </div>
       <div className={styles.progressbar}>
         <div className={styles.rating}>1 star </div>
         <ProgressBar percentage={one} rating={1} />
-        <div className={styles.percent}> {stringPercent(one)}% </div>
+        <div className={styles.percent}> {stringPercent(one)}</div>
       </div>
       <All qty={aggregates.qty} />
     </div>
